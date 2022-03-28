65abb8773abbee9b9f5c71cf8267e4b6303a6a59
shivampal-hub <70017082+shivampal-hub@users.noreply.github.com>
2022-03-28 23:44:34 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -5,7 +5,7 @@ public class Palindrome {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to reverse");
+        System.out.println("Enter a number to find Palindrome");
         n = sc.nextInt();
         original = n;
         reverse();
